ffa93ed853bd91e36e92baed6dc47ecbfdde788d
shadysima <nschultz4645@gmail.com>
2021-05-20 09:58:26 -0400

    updated DWM autostart script and added battery script

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-res &
 nitrogen --restore &
 compton &
 
-dte() {
+bar() {
   dte=$(date +"%d %b %Y (%A), %H:%M:%S")
-  echo -e "$dte"
+  bat=$(cat /sys/class/power_supply/BAT0/capacity)
+  status=$(cat /sys/class/power_supply/BAT0/status)
+
+  if [[ "$status" == "Discharging" ]]; then
+	  state="D"
+  else
+	  state="C"
+  fi
+
+
+
+  echo -e "BAT: $bat% $state | $dte"
 }
 
 while true; do
-  xsetroot -name "$(dte)"
+  xsetroot -name "$(bar)"
   sleep 1
 done &
